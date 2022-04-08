9c22ceeccfea401d1d669dd28583cdeb44b45ca0
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-08 18:16:06 -0300

    resolvendo alguns exercicios do bloco 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/2-exercicios/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/2-exercicios/script.js
@@ -107,3 +107,15 @@
 
 // 1- Ordene o array numbers em ordem crescente e imprima seus valores;
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let position1 = [];
+
+for(index = 1; index < numbers.length; index += 1) {
+    for(index2 = 0; index2 < index; index2 += 1) {
+        if(numbers[index] < numbers[index2]) {
+            let position = position1.push(numbers[index]);
+        }
+    }
+}
+
+console.log(position1);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3/1-conteudos/script.js
@@ -0,0 +1,24 @@
+// 1- Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
+
+// Adicionar o array;
+// Criar uma variável com valor 0;
+// Criar um loop que percorre o array;
+// Incrementar a variável com o valor correspondente a cada loop;
+// Criar um if com a condição da variável ser maior que 15;
+// Caso a variável obedeça a condição;
+// Imprimir a variável
+// Caso não obedeça a condição;
+// Imprimir a mensagem "valor menor que 16";
+
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+    soma += fruits[index]; //é a mesma coisa que "soma = soma + fruits[index];""
+  }
+
+if(soma > 15) {
+    console.log(soma);
+} else {
+    console.log('valor menor que 16');
+}
