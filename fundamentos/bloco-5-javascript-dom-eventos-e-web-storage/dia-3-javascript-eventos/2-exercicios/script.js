02449d447c79943b6710c6d66f649fe97d44220b
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-19 22:28:33 -0300

    concluído os exercícios do bloco 5.3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/2-exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/2-exercicios/script.js
@@ -18,72 +18,76 @@ createDaysOfTheWeek();
 //   Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
 
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const listDays = document.querySelector('#days');
 
-for (index = 0; index < dezDaysList.length; index += 1) {
-  let daysDez = dezDaysList[index];
-  let daysDezItem = document.createElement('li');
-  daysDezItem.innerHTML = daysDez;
+function createDaysList() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const listDays = document.querySelector('#days');
+  for (index = 0; index < dezDaysList.length; index += 1) {
+    let daysDez = dezDaysList[index];
+    let daysDezItem = document.createElement('li');
+    daysDezItem.innerHTML = daysDez;
 
-  listDays.appendChild(daysDezItem);
-  daysDezItem.className = "day"
+    listDays.appendChild(daysDezItem);
+    daysDezItem.className = "day"
 
-  // Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+    // Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
 
-  if (index === 25 | index === 32) {
-    daysDezItem.className = "day holiday"
-  }
-  // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
-  if (index === 5 | index === 12 | index === 19) {
-    daysDezItem.className = 'day friday'
-  }
-  if (index === 26) {
-    daysDezItem.className = 'day holiday friday'
+    if (index === 25 | index === 32) {
+      daysDezItem.className = "day holiday"
+    }
+    // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+    if (index === 5 | index === 12 | index === 19) {
+      daysDezItem.className = 'day friday'
+    }
+    if (index === 26) {
+      daysDezItem.className = 'day holiday friday'
+    }
   }
 }
+createDaysList();
+
 // Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
 // Adicione a este botão a ID "btn-holiday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 
-  function holidayButton (string) {
+function holidayButton(string) {
   let buttonHoliday = document.createElement('button');
   let buttonsContainer = document.querySelector('.buttons-container');
   buttonsContainer.appendChild(buttonHoliday);
   buttonHoliday.innerText = string;
   buttonHoliday.setAttribute('id', 'btn-holiday'); //ref: https://www.educative.io/edpresso/how-to-add-an-id-to-element-in-javascript// como criar uma id pelo js
-  }
+}
 
 holidayButton('Feriados');
 
 // Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
 // É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
-function backgroundColorHoliday () {
-let holiday = document.getElementsByClassName('holiday');
-let botaoFeriado = document.getElementById('btn-holiday');
-botaoFeriado.addEventListener('click', function (){
-  holiday[0].style.backgroundColor = "green";
-  holiday[1].style.backgroundColor = "green";
-  holiday[2].style.backgroundColor = "green";
-})
-botaoFeriado.addEventListener('dblclick', function (){
-  holiday[0].style.backgroundColor = "rgb(238,238,238)";
-  holiday[1].style.backgroundColor = "rgb(238,238,238)";
-  holiday[2].style.backgroundColor = "rgb(238,238,238)";
-})
+function backgroundColorHoliday() {
+  let holiday = document.getElementsByClassName('holiday');
+  let botaoFeriado = document.getElementById('btn-holiday');
+  botaoFeriado.addEventListener('click', function () {
+    holiday[0].style.backgroundColor = "green";
+    holiday[1].style.backgroundColor = "green";
+    holiday[2].style.backgroundColor = "green";
+  })
+  botaoFeriado.addEventListener('dblclick', function () {
+    holiday[0].style.backgroundColor = "rgb(238,238,238)";
+    holiday[1].style.backgroundColor = "rgb(238,238,238)";
+    holiday[2].style.backgroundColor = "rgb(238,238,238)";
+  })
 }
-backgroundColorHoliday ();
+backgroundColorHoliday();
 
 // Exercício 4:
 // Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
 // Adicione a este botão o ID "btn-friday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container"
-function fridayButton (string) {
-let botaoSexta = document.createElement('button');
-let buttonsContainer = document.querySelector('.buttons-container');
-buttonsContainer.appendChild(botaoSexta);
-botaoSexta.innerText = string;
-botaoSexta.setAttribute('id', 'btn-friday')
+function fridayButton(string) {
+  let botaoSexta = document.createElement('button');
+  let buttonsContainer = document.querySelector('.buttons-container');
+  buttonsContainer.appendChild(botaoSexta);
+  botaoSexta.innerText = string;
+  botaoSexta.setAttribute('id', 'btn-friday')
 }
 
 fridayButton('Sexta-Feira');
@@ -92,11 +96,18 @@ fridayButton('Sexta-Feira');
 // Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
 // É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
 
-let botaoSexta = document.querySelector('#btn-friday');
-botaoSexta.addEventListener('click', function () {
-  let friday = document.getElementsByClassName('friday');
-  friday[5].innerText('Sexta');
-  friday[12].innerText('Sexta');
-  friday[19].innerText('Sexta');
-  friday[26].innerText('Sexta');
-})
+function addSextaASexta() {
+  let botaoSexta = document.getElementById('btn-friday');
+  botaoSexta.addEventListener('click', function () {
+    const friday = document.getElementsByClassName('friday');
+    const fridayNumber = [4, 11, 18, 25];
+    for (let index = 0; index < friday.length; index += 1) {
+      if (friday[index].innerHTML !== 'Sexta') {
+        friday[index].innerHTML = 'Sexta';
+      } else {
+        friday[index].innerHTML = fridayNumber[index];
+      }
+    }
+  });
+}
+addSextaASexta();
