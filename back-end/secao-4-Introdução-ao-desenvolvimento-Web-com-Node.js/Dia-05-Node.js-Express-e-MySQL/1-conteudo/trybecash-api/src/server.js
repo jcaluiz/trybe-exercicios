03e7579cb6a684a56f3a147aca3b4ae9340bc23a
Luiz Junior <lacjunior129.la@gmail.com>
2022-10-18 22:14:19 -0300

    backend 4.5

---
+++ back-end/secao-4-Introdução-ao-desenvolvimento-Web-com-Node.js/Dia-05-Node.js-Express-e-MySQL/1-conteudo/trybecash-api/src/server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+
+require('dotenv').config();
+
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`API TrybeCash está sendo executada na porta ${port}`);
+});
